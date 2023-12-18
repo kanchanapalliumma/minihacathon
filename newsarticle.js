ceecbafecdba2ee0eb30230d21fa31e53fb800f2
kanchanapalliumma <kanchanapalliuma22@navgurukul.org>
2023-12-18 11:58:19 +0530

    first commit

--- newsarticle.js
+++ newsarticle.js
@@ -1,6 +1,7 @@
 function showdata(attaching){
     const url= `https://newsapi.org/v2/everything?q=bitcoin&apiKey=68791b949ff04702b17a84ed88c84e02`
     const fetchurl=(attaching==="")  ?url  :`https://newsapi.org/v2/everything?q=${attaching}&from=2023-12-16&to=2023-12-16&sortBy=popularity&apiKey=68791b949ff04702b17a84ed88c84e02`
+    const newone=document.getElementById("arrow")
 
     fetch(fetchurl)
     .then((response)=>response.json())
@@ -14,7 +15,8 @@ function showdata(attaching){
            if (image){
            const title=item.title
            const description=item.description
-           newshtml+=`<li><img src="${image}">  <h3>${title}</h3>  <p>${description}</p></li>`
+           const newdescription=description.slice(0,100)+"......."
+           newshtml+=`<li><img src="${image}">  <h3>${title}</h3>  <p>${newdescription}</p> <p id="arrow">Read full article  <a href="${item.url}" target="_blank" class="read-article"><span>&#8594;</span></a></p></li>`
            count=count+1
            }
            }
